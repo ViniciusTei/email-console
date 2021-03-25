afc088e5a6010431f93d07b004fb3657885131fb
vinicius.teixeira <vinicius.teixeira@mmtools.com.br>
2021-03-25 11:56:31 -0300

    :bug: fix compiling error after install cli

--- email.js
+++ email.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const sgMail = require('@sendgrid/mail');
 const API_KEY = require('./sendgrid.env').API_KEY;
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const prompt = require('prompt-sync')();
 const sendEmail = require('./email')
 
